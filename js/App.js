c359f3ffe8fa018e67fe858065daf08956756300
Morgane Lejeune <morgane.lejeune@hotmail.fr>
2022-10-17 14:41:18 +0200

    #3 - feat :  finishing constructor pattern of new-movie.js, create link in index.html, app.js

--- js/App.js
+++ js/App.js
@@ -1,20 +1,15 @@
 class App {
     constructor() {
         this.$moviesWrapper = document.querySelector('.movies-wrapper')
-        this.moviesApi = new MovieApi('/data/movie-data.json')
+        this.moviesApi = new MovieApi('/data/new-movie-data.json')
     }
 
     async main() {
         const moviesData = await this.moviesApi.getMovies()
 
         moviesData
-            .map(movie => new OldMovie(movie))
+            .map(movie => new Movie(movie))
             .forEach(movie => {
-
-              console.log("===")
-              console.log(movie)
-              console.log("===")
-
               const Template = new MovieCard(movie)
               this.$moviesWrapper.appendChild(Template.createMovieCard())
         })
